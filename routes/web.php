40958551c19b9875d9f4368575e7a3eb9da6dfd4
rwierucki <rwierucki@gmail.com>
2021-02-24 00:56:08 +0100

    loyouts, blade, route

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,28 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $post = [
+        (object)[
+            'id' => 1,
+            'title' => 'Test 1',
+            'content' => '<b>Test bold</b> cos innego Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto culpa dolor eos error esse exercitationem expedita id in nesciunt nihil numquam optio, perferendis qui ut vero! Asperiores incidunt quod tempore?',
+            'date' => '2021-02-02',
+            'type' => 'text',
+            'image' => null
+        ],
+        (object)[
+            'id' => 2,
+            'title' => 'Test photo ',
+            'content' => '<b>Test bold</b> cos innego Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto culpa dolor eos error esse exercitationem expedita id in nesciunt nihil numquam optio, perferendis qui ut vero! Asperiores incidunt quod tempore?',
+            'date' => '2021-02-02',
+            'type' => 'photo',
+            'image' => '/images/image-01.jpg'
+        ],
+
+    ];
+    return view('pages.posts');
 });
+
+Route::get('/about-me', function () {
+    return view('pages.about');
+})->name('about');
